efb8adc6ac957ae72f8ce8331f3d2ff877f8f93a
SanekP <sanekne@mail.ru>
2014-08-16 22:00:38 +0300

    Changed access type to property

--- UI/src/main/java/sanekp/seriesinformer/ui/xml/SeriesList.java
+++ UI/src/main/java/sanekp/seriesinformer/ui/xml/SeriesList.java
@@ -1,7 +1,5 @@
 package sanekp.seriesinformer.ui.xml;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,14 +8,17 @@ import java.util.List;
  * Created by sanek_000 on 5/25/2014.
  */
 @XmlRootElement(name = "list")
-@XmlAccessorType(XmlAccessType.FIELD)
 public class SeriesList {
     private List<Series> series;
 
     public List<Series> getSeries() {
         if (series == null) {
-            series = new ArrayList<Series>();
+            series = new ArrayList<>();
         }
         return series;
+    }
+
+    public void setSeries(List<Series> series) {
+        this.series = series;
     }
 }
